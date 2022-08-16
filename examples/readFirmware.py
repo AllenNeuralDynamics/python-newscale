fffb20b3a354e66c6c02c8185253f1e8505b6ab9
Chris Chronopoulos <chronopoulos.chris@gmail.com>
2022-08-15 18:19:43 -0700

    add M3_LS_34_15_XYZ_PoE class and example script

---
+++ examples/readFirmware.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import socket
+from newscale.stages import M3_LS_34_15_XYZ_PoE as Stage
+
+IP = '10.128.49.22'
+PORT = 23
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((IP, PORT))
+
+stage = Stage(sock)
+fw_version = stage.readFirmwareVersion()
+print('Firmware Version: ', fw_version)
+stage.close()
+
