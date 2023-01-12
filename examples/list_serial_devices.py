791dddec2cbc9a47d68d81f138feb3efbb949226
Chris Chronopoulos <chris.chronopoulos@alleninstitute.org>
2023-01-11 16:47:58 -0800

    examples/list_serial_devices.py: fix bug

--- examples/list_serial_devices.py
+++ examples/list_serial_devices.py
@@ -4,4 +4,4 @@ from newscale.interfaces import NewScaleSerial
 
 instances = NewScaleSerial.get_instances()
 for i in instances:
-    print('Serial #%d, on %s', (i.get_serial_number(), i.get_port_name()))
+    print('Serial # %s, on %s' % (i.get_serial_number(), i.get_port_name()))
